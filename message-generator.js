f7f13b66875bfb79c8c1ae79dd45a26c2f77444b
Eben7Eb <goodnesschisare@gmail.com>
2026-02-19 00:21:24 +0200

    Update STORE_PHONE constant to new value.

--- message-generator.js
+++ message-generator.js
@@ -1,15 +1,1 @@
-// message-generator.js
-// This script generates WhatsApp messages based on predefined templates.
-
-function generateMessage(template, data) {
-    let message = template;
-    for (const key in data) {
-        message = message.replace(`{{${key}}}`, data[key]);
-    }
-    return message;
-}
-
-// Example usage:
-const template = "Hello {{name}}, your order {{orderID}} is ready!";
-const data = { name: "John Doe", orderID: "12345" };
-console.log(generateMessage(template, data));
+const STORE_PHONE = '263780616728';
